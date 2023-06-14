59038f7d85aa7c1dfbb731b01d77714617576eba
moemen gaballa <moemengaballa@gmail.com>
2023-06-14 22:26:35 +0300

    implement basic Event Bus

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require('cors')
+const axios = require('axios');
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,7 +16,7 @@ app.get('/posts', (req, res) => {
 });
 
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const  id = randomBytes(4).toString('hex');
 
     const { title } = req.body;
@@ -22,6 +24,13 @@ app.post('/posts', (req, res) => {
     posts[id] = {
         id, title
     };
+
+    await axios.post('http://localhost:4005/events', {
+       type: 'PostCreated',
+       'data': {
+           id, title
+       }
+    });
 
     res.status(201).send(posts[id]);
 });
